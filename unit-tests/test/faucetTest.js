3b4ce0902757189e811181e0ab725f5cb7d5464d
Jenny <jennyliu07@gmail.com>
2023-06-05 18:08:01 +0800

    unit test / better implementation

--- unit-tests/test/faucetTest.js
+++ unit-tests/test/faucetTest.js
@@ -34,22 +34,41 @@ describe("Faucet", function () {
   // CHALLENGE: The destroyFaucet function should be able to called by the contract owner
   // as should the withdrawAll function
 
-  it("Should only allow the owner to call destroryFaucet", async function () {
+  it("Should only allow the owner to call destroyFaucet and it should self destruct", async function () {
     const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
+
     const [, nonOwner] = await ethers.getSigners();
 
     // Expect that calling destroyFaucet as a non-owner will be reverted
     await expect(faucet.connect(nonOwner).destroyFaucet()).to.be.reverted;
 
     // Expect that calling destroyFaucet as the owner will succeed
-    await expect(faucet.connect(owner).destroyFaucet()).to.not.be.reverted;
+    await faucet.connect(owner).destroyFaucet();
+
+    // https://docs.ethers.org/v5/single-page/#/v5/api/providers/provider/-%23-Provider-getCode
+    const code = await ethers.provider.getCode(faucet.address);
+    expect(code).to.equal("0x");
   });
 
-  it("Should only allow the owner to call withdraw", async function () {
+  it("Should only allow the owner to call withdrawAll and it should return all ether", async function () {
     const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
     const [, nonOwner] = await ethers.getSigners();
 
     await expect(faucet.connect(nonOwner).withdrawAll()).to.be.reverted;
-    await expect(faucet.connect(owner).withdrawAll()).to.not.be.reverted;
+
+    // Set the initial balance to 1 Ether
+    const initialBalance = ethers.utils.parseEther("1");
+
+    // Call withdrawAll as the owner
+    await faucet.connect(owner).withdrawAll();
+
+    // Get the contract balance after withdrawal
+    const finalBalance = await ethers.provider.getBalance(faucet.address);
+    expect(finalBalance).to.equal(0);
+
+    // Check that the owner's balance has increased by the initial balance of the contract
+    const ownerBalanceAfter = await ethers.provider.getBalance(owner.address);
+    const ownerBalanceBefore = ownerBalanceAfter.sub(initialBalance);
+    expect(ownerBalanceBefore).to.be.lte(ownerBalanceAfter);
   });
 });
