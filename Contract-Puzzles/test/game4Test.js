5098aca5bf414f4a194baf5eb1c8d9736cf7c09e
Jenny <jennyliu07@gmail.com>
2023-06-25 14:30:44 +0800

    contract puzzles - solution

--- Contract-Puzzles/test/game4Test.js
+++ Contract-Puzzles/test/game4Test.js
@@ -1,21 +1,27 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
+const { ethers } = require("hardhat");
 
-describe('Game4', function () {
+describe("Game4", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game4');
+    const Game = await ethers.getContractFactory("Game4");
     const game = await Game.deploy();
 
-    return { game };
+    const singer = ethers.provider.getSigner(0);
+    const address = await singer.getAddress();
+
+    return { game, singer, address };
   }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+  it("should be a winner", async function () {
+    const { game, singer, address } = await loadFixture(
+      deployContractAndSetVariables,
+    );
 
     // nested mappings are rough :}
-
-    await game.win();
+    await game.connect(singer).write(address);
+    await game.connect(singer).win(address);
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
--- Contract-Puzzles/test/game5Test.js
+++ Contract-Puzzles/test/game5Test.js
@@ -1,21 +1,48 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
+const { ethers } = require("hardhat");
 
-describe('Game5', function () {
+describe("Game5", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
+    const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
 
     return { game };
   }
-  it('should be a winner', async function () {
+  it("should be a winner", async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
+    const provider = game.provider;
+    const threshold = ethers.BigNumber.from(
+      "0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf",
+    );
+    const [deployer] = await ethers.getSigners();
 
-    await game.win();
+    let winnerWallet;
+    for (let i = 0; i < 100000; i++) {
+      // try 100000 times
+      const randomWallet = ethers.Wallet.createRandom().connect(provider);
+      if (ethers.BigNumber.from(randomWallet.address).lt(threshold)) {
+        winnerWallet = randomWallet;
+        break;
+      }
+    }
+
+    // check if we found a winner address
+    assert(winnerWallet, "Failed to find a winner wallet");
+
+    // send some Ether to the winner wallet
+    await deployer.sendTransaction({
+      to: winnerWallet.address,
+      value: ethers.utils.parseEther("1"), // 1 Ether
+    });
+
+    // use the winner address to win the game
+    const gameWithWinner = game.connect(winnerWallet);
+    await gameWithWinner.win();
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
